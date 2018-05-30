436ae0b5d2571cd8e2664826f04fcfa34ef7d51a
aexol <aexol@aexol.com>
2018-05-30 18:45:01 +0200

    added cors support headers

--- src/index.ts
+++ src/index.ts
@@ -58,27 +58,40 @@ const middlewareParser = (
   return run;
 };
 const run: SlothkingRunner = (extensions, databaseURL) => async (req, res) => {
-  res.setHeader("Access-Control-Max-Age", `${3600 * 24}`);
+  const DEFAULT_ALLOW_METHODS = [
+    "POST",
+    "GET",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS"
+  ];
+
+  const DEFAULT_ALLOW_HEADERS = [
+    "X-Requested-With",
+    "Access-Control-Allow-Origin",
+    "X-HTTP-Method-Override",
+    "Content-Type",
+    "Authorization",
+    "Accept"
+  ];
+
+  const DEFAULT_MAX_AGE_SECONDS = 60 * 60 * 24; // 24 hours
+
+  res.setHeader("Access-Control-Max-Age", DEFAULT_MAX_AGE_SECONDS);
+
   res.setHeader("Access-Control-Allow-Origin", "*");
+
   res.setHeader(
     "Access-Control-Allow-Methods",
-    ["POST", "GET", "PUT", "PATCH", "DELETE", "OPTIONS"].join(",")
+    DEFAULT_ALLOW_METHODS.join(",")
   );
+
   res.setHeader(
     "Access-Control-Allow-Headers",
-    [
-      "X-Requested-With",
-      "Access-Control-Allow-Origin",
-      "X-HTTP-Method-Override",
-      "Content-Type",
-      "Authorization",
-      "Accept"
-    ].join(",")
+    DEFAULT_ALLOW_HEADERS.join(",")
   );
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  if (req.method === "OPTIONS") {
-    return {};
-  }
   if (!connectedToDatabase && databaseURL) {
     connect(databaseURL);
     connectedToDatabase = true;
